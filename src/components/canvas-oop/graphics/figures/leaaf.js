ffd8ee2344b89f6a084ce7f27f0947b6a5278702
Marina Sidorkina <m.sidorkina55@gmail.com>
2022-10-28 15:17:00 +0300

    Add animation variants.

--- src/components/canvas-oop/graphics/figures/leaaf.js
+++ src/components/canvas-oop/graphics/figures/leaaf.js
@@ -19,6 +19,12 @@ class Leaf extends BaseRectangle {
     this.width = (this.params[this.mod].width * this.pxl) / this.divider;
     this.height = (this.params[this.mod].height * this.pxl) / this.divider;
     this.bottomOffset =  (this.params[this.mod].height * this.pxl) / this.divider;
+    this.angle = 0;
+    this.animationFunctions = {
+      1: this.swing,
+      2: this.rotate,
+    }
+    this.animationVariant = this.animationFunctions[generateRandomNumber(1, 2)]
   }
 
   /**
@@ -27,11 +33,24 @@ class Leaf extends BaseRectangle {
    * @param bottom {number}
    */
   animate(time, bottom) {
+    this.dt = (time - this.time) / 1000;
+    this.animationVariant();
+
     if (this.y < bottom) {
       this.y += 1;
+      this.x += Math.sin(this.dt);
     } else {
       this.y = -200;
     }
+  }
+
+  swing() {
+    this.angle += Math.sin(this.dt);
+  }
+
+  rotate() {
+    this.angle += 1;
+    if (this.angle > 360) this.angle = 0;
   }
 
   /**
@@ -45,6 +64,9 @@ class Leaf extends BaseRectangle {
 
     ctx.save();
     if (action.index === this.index || action.follow === this.index) ctx.strokeStyle = 'green';
+    ctx.translate(this.x + this.width/2, this.y + this.height/2);
+    ctx.rotate(this.angle * Math.PI / 180 );
+    ctx.translate(-(this.x + this.width/2), -(this.y + this.height/2));
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     if (action.index === this.index || action.follow === this.index) ctx.strokeRect(this.x, this.y, this.width, this.height);
     ctx.restore();
